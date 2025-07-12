491ec6dc538f391a3bcae785c32da97b2d28734d
moham <moham@DESKTOP-M8CT1H1>
2025-07-12 09:26:43 +0300

    Q09

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -243,6 +243,20 @@
             //Console.WriteLine(Name); 
             #endregion
 
+            #region Q09
+
+            //Console.Write("Enter The Value Of N : ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //        Console.Write(n);
+            //    Console.WriteLine();
+            //} 
+            #endregion
+
+
         }
     }
 }
