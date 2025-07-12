e6ff5318b954a2c614ad3ad6fd5842d437584a63
moham <moham@DESKTOP-M8CT1H1>
2025-07-12 09:44:11 +0300

    Two D Array

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -48,6 +48,57 @@
             //Console.WriteLine(Numbers.Length);
             //Console.WriteLine(Numbers.Rank); 
             #endregion
+
+            #region Two D Array
+            ////Two D Array
+
+            //int[,] Marks = new int[3,3];
+            ////Marks[0,0] = 99;
+            ////Marks[0, 1] = 52;
+            ////Marks[0, 2] = 98;
+            ////Marks[1, 0] = 7;
+            ////Marks[1, 1] = 58;
+            ////Marks[1, 2] = 98;
+
+            ////Console.WriteLine(Marks[0,0]);
+            ////Console.WriteLine(Marks[0,1]);
+            ////Console.WriteLine(Marks[0,2]);
+            ////Console.WriteLine(Marks[1,0]);
+            ////Console.WriteLine(Marks[1,1]);
+            ////Console.WriteLine(Marks[1,2]);
+
+            ////for (int i = 0; i < 3; i++)
+            ////{
+            ////    for(int j = 0; j < 3; j++)
+            ////    {
+            ////        Console.Write($"Enter Of Mark [{i},{j}] :  ");
+            ////        Marks[i,j] = int.Parse( Console.ReadLine() );
+            ////    }
+            ////}
+
+            ////for (int i = 0; i < 3; i++)
+            ////{
+            ////    for (int j = 0; j < 3; j++)
+            ////    {
+            ////        Console.Write($"{Marks[i,j]} ");
+
+            ////    }
+            ////    Console.WriteLine();
+            ////}
+            ////int x=0, y=0;
+            ////for (int i = 0;i<Marks.Length; i++)
+            ////{
+            ////    if (y == 3)
+            ////    {
+            ////        x++;
+            ////        y = 0;
+            ////        Console.WriteLine();
+            ////    }
+            ////    Console.Write($"{Marks[x, y]} ");
+            ////    y++;
+
+            ////} 
+            #endregion
         }
     }
 }
